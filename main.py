55ae2695ab59ef257fc77e64cd2628468b81857f
Owen Hill <47730337+thisisowenhill@users.noreply.github.com>
2022-09-10 23:48:05 -0500

    Added window functions and fixed small things
    I have yet to test whether or not the grabbing of yesterday's information works.
    TODO: Make the grabbing of yesterdays information work for exampleTwo()
    
    Fixed case default to case _ (the real default)
    Created a folder to store all of the saved data for easier file operations
    Search bar that pulls the data for the listed stock

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 from urllib.error import HTTPError
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
@@ -15,48 +17,76 @@ def sortFile(raw):
     sorted_df.columns = ["Sym", "SVol", "SXVol", "TVol", "Mrkt"]
     for row in sorted_df.itertuples(index=True, name="Model"):
         percent = (row[2] / row[4]) * 100
-        if not(65 >= percent >= 40 and row[2] > 100 and row[4] > 10000 and (row[3] / row[2]) * 100 >= 1):
+        if not (65 >= percent >= 40 and row[2] > 100 and row[4] > 10000 and (row[3] / row[2]) * 100 >= 1):
             # remove the stock from the list if the following requirements aren't met:
             #  - short volume is within 40%-65% of the total volume
             #  - short exempt volume is more than 100
             #  - total volume exceeds 10,000
             sorted_df.drop(row[0], inplace=True)
     sorted_df.sort_values("TVol", ascending=False, inplace=True)
-    print(sorted_df)
+
     return sorted_df
 
 
 def createFile(url):
+    date = re.search("\d+", url)
     req = Request(url=url, headers={'User-Agent': 'XYZ/3.0'})
     page = urlopen(req, timeout=10).read()
     webpage = BeautifulSoup(page, "html.parser")
-    with open("raw.txt", "w") as file:
+    with open("finra/{}.txt".format(date.group()), "w") as file:
         file.write(str(webpage))
-    df = pd.read_csv("raw.txt", delimiter="|")
+    df = pd.read_csv("finra/{}.txt".format(date.group()), delimiter="|")
     new_df = sortFile(df)
-    new_df.to_csv("fin.csv", index=None)
-    new_df.to_excel("fin.xlsx", sheet_name="Seet")
+    new_df.to_csv("finra/{}.csv".format(date.group()), index=None)
+
+    files = sorted(Path("finra/").glob("[0-9]*"))
+    if len(files) >= 8:
+        try:
+            os.remove(files[0])
+            os.remove(files[0])
+        except FileNotFoundError:
+            pass
+    elif len(files) >= 4:
+        text = re.search("\d+", files[:-3].__str__())
+        return date.group(), text.group()
+
+    return date.group()
 
 
-def createWindow():
-    df = pd.read_csv('fin.csv', delimiter=',')
+def createWindow(date, file):
+    df = pd.read_csv("finra/{}.csv".format(date), delimiter=',')
+    df2 = pd.read_csv("finra/{}.csv".format(file))
     csv = [list(row) for row in df.values]
     csv.insert(0, df.columns.to_list())
     layout = [
+        [sg.Input(key="_input_"),
+         sg.Button(button_text="search", enable_events=True)],
+        [sg.Text(text="",
+                 enable_events=True,
+                 key="_output_")],
         [sg.Table
-         (csv,
+         (csv[1:],
           headings=["Sym", "SVol", "SXVol", "TVol", "Mrkt"],
           selected_row_colors=("white", "gray"),
           expand_y=True,
           enable_events=True,
-          right_click_selects=True)
-         ]
+          right_click_selects=True,
+          key="_table_")
+         ],
+        [sg.Text(text="Total filtered stocks: " + str(len(df))),
+         sg.Text(text="Previous days filtered stocks: " + str(len(df2)))]  #  TODO
     ]
 
-    window = sg.Window("CSV Viewer", layout, size=(500, 300))
+    window = sg.Window("CSV Viewer", layout, size=(500, 300), resizable=True, element_justification="center")
     while True:
         event, values = window.read()
-        if event == sg.WIN_CLOSED:
+
+        if event == "search" and values["_input_"] != "":
+            query = values["_input_"].upper()
+            print(len(df))
+            if query in df["Sym"].values:
+                window.Element("_output_").update(df.loc[df["Sym"] == query])
+        elif event == sg.WIN_CLOSED:
             break
 
 
@@ -68,8 +98,7 @@ def exampleOne():
     for text in soup.findAll('a', attrs={'href': re.compile("^https://")}):
         link = text.get('href')
         if "txt" in link:
-            createFile(link)
-            break
+            return createFile(link)
 
 
 def exampleTwo():
@@ -96,7 +125,7 @@ def exampleTwo():
             new_date = date.replace(day=date.day - 2)
             createFile("https://cdn.finra.org/equity/regsho/daily/CNMSshvol{}.txt"
                        .format(new_date.strftime("%Y%m%d")))
-        case default:
+        case _:
             try:
                 createFile("https://cdn.finra.org/equity/regsho/daily/CNMSshvol{}.txt".format(date.strftime("%Y%m%d")))
             except HTTPError:
@@ -107,5 +136,10 @@ def exampleTwo():
 
 
 if __name__ == '__main__':
-    exampleOne()
-    createWindow()
+    try:
+        os.mkdir("finra")
+    except FileExistsError:
+        pass
+
+    file_name, files = exampleOne()
+    createWindow(file_name, files)
